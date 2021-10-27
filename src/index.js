b9bfe88298af3745ef3b05954c309192c09abecd
Karol Wasemann <kw.kwaq@gmail.com>
2021-10-27 18:41:31 +0200

    header + skills section

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-container");
+
+burger.addEventListener("click", function () {
+    nav.classList.toggle('toggler');
+    console.log("ga;")
+
+})
